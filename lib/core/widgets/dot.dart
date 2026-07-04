4c17a25cf6222b4e5be7d059705f3b9afd36fc8c
Meriem <om_semsoum@esi.dz>
2026-07-04 17:55:31 +0100

    added dot, image

---
+++ lib/core/widgets/dot.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Dot extends StatelessWidget {
+  final double size;
+  final Color color;
+  final double opacity;
+
+  const Dot({
+    super.key,
+    required this.size,
+    required this.color,
+    required this.opacity,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: color.withOpacity(opacity),
+      ),
+    );
+  }
+}
---
+++ lib/features/screens/auth/splash_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import '../../../core/constants/colors.dart';
+import '../../../core/constants/styles.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.backgroundlightColor,
+    );
+  }
+}
