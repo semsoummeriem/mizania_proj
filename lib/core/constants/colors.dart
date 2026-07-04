fe9fd084dd5da9e0c8651d11393d3fce8c9b26e1
Meriem <om_semsoum@esi.dz>
2026-07-04 14:15:36 +0100

    Initial commit

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundlightColor = Color(0xFFF5F5F7);
+  static const Color smalltextColor = Color(0xFF8B8898);
+  static const Color dotColor = Color(0xFF7F77DD);
+  static const Color bigtextColor = Color(0xFF000000);
+  static const Color darkmauveColor = Color(0xFF534AB7);
+  static const Color blueColor = Color(0xFF378ADD);
+  static const Color roseColor = Color(0xFFD4537E);
+  static const Color amberColor = Color(0xFFBA7517);
+  static const Color backgrounddarkColor = Color(0xFF0F0A2A);
+  static const TextStyle bigtextstyle = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w600,
+    color: AppColors.bigtextColor,
+    fontFamily: 'PlusJakartaSans',
+  );
+  static const TextStyle smalltextstyle = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+    color: AppColors.smalltextColor,
+    fontFamily: 'PlusJakartaSans',
+  );
+}
