a5ebab942add1f17d8202cc2251e2f12aad85531
Meriem <om_semsoum@esi.dz>
2026-07-17 16:00:47 +0100

    new changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,15 @@ import 'core/state/app_state.dart';
 import 'core/state/app_state_scope.dart';
 import 'features/screens/auth/onboarding.dart';
 import 'package:mizania_proj/features/screens/auth/splash_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://eaoqbouxgkutrkrtjacb.supabase.co',
+    publishableKey: 'sb_publishable_0bpLfrk24_WWWZJeMsC1oQ_EwcoR7ZS',
+  );
+
   final appState = AppState();
 
   runApp(
